2fec8467fb873eb52177fb21c8f276ac0b9d4e59
s-choory <135598730+s-choory@users.noreply.github.com>
2023-12-04 20:43:06 +0900

    Update BOJ_28279[덱2].java

--- Java/Deque/BOJ_28279[덱2].java
+++ Java/Deque/BOJ_28279[덱2].java
@@ -1,106 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
 	
+	static Deque<Integer> deque = new ArrayDeque<>();
+	static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws IOException {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
-		Deque deque = new Deque();
+		while(n-- > 0) {
+			solution(br.readLine());
+		}
+		br.close();
 		
-		for (int i = 0; i < n; i++) {
-			String s = br.readLine();
-			
-			if(s.charAt(0) == '1') {
-				deque.pushFront(Integer.parseInt(s.substring(2)));
-			}
-			else if(s.charAt(0) == '2') {
-				deque.pushBack(Integer.parseInt(s.substring(2)));
-			}
-			else if(s.charAt(0) == '3') {
-				System.out.println(deque.popFront());
-			}
-			else if(s.charAt(0) == '4') {
-				System.out.println(deque.popback());
-			}
-			else if(s.charAt(0) == '5') {
-				System.out.println(deque.size());
-			}
-			else if(s.charAt(0) == '6') {
-				if(deque.isEmpty()) {
-					System.out.println(1);
-				}else {
-					System.out.println(0);
-				}
-			}
-			else if(s.charAt(0) == '7') {
-				if(deque.isEmpty()) {
-					System.out.println("-1");
-				}else {
-					System.out.println(deque.getFront());
-				}
-			}
-			else if(s.charAt(0) == '8') {
-				if(deque.isEmpty()) {
-					System.out.println("-1");
-				}else {
-					System.out.println(deque.getBack());
-				}
-			}
-		}
-	}	
-	
-	public static class Deque {
-		LinkedList<Integer> deque = new LinkedList<>();
-		
-
-		public void pushFront(int x){
-			deque.addFirst(x);
-		}
-		
-		public void pushBack(int x){
-			deque.addLast(x);
-		}
-		
-		public String popFront() {
-			if(deque.isEmpty()) {
-				return "-1";
-			}
-			return String.valueOf(deque.pollFirst());
-		}
-
-		public String popback() {
-			if(deque.isEmpty()) {
-				return "-1";
-			}
-			return String.valueOf(deque.pollLast());
-		}
-		
-		public int size() {
-			return deque.size();
-		}
-		
-		public int getFront() {
-			if(deque.isEmpty()) {
-				return -1;
-			}
-			return deque.getFirst();
-		}
-
-		public int getBack() {
-			if(deque.isEmpty()) {
-				return -1;
-			}
-			return deque.getLast();
-		}
-		
-		public boolean isEmpty() {
-			return deque.isEmpty();
+		System.out.println(sb);
+	}
+	static void solution(String query) {
+		char c = query.charAt(0);
+		switch (c) {
+		case '1':deque.addFirst(Integer.parseInt(query.substring(2)));return;
+		case '2':deque.addLast(Integer.parseInt(query.substring(2)));return;
+		case '3':sb.append((deque.isEmpty()?"-1":deque.pop())+"\n");return;
+		case '4':sb.append((deque.isEmpty()?"-1":deque.pollLast())+"\n");return;
+		case '5':sb.append((deque.size())+"\n");return;
+		case '6':sb.append((deque.isEmpty()?"1":"0")+"\n");return;
+		case '7':sb.append((deque.isEmpty()?"-1":deque.peekFirst())+"\n");return;
+		case '8':sb.append((deque.isEmpty()?"-1":deque.peekLast())+"\n");return;
+		default:break;
 		}
 	}
 }
-
